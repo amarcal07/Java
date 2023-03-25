42d14ac790e5392ec776b6eeca1fb95a7f4f33d2
Andresa Marçal <andresamarques3107@gmail.com>
2023-03-24 22:06:41 -0300

    Subir Orientação Objeto

---
+++ orientacao_objeto/src/orientacao_objeto/TestaCliente.java
@@ -0,0 +1,19 @@
+package orientacao_objeto;
+
+public class TestaCliente {
+	
+	public static void main(String[] args) {
+		
+		  Exer01_ClasseCliente tc = new Exer01_ClasseCliente ("Andresa" , 26, "Feminino" , "Rio de Janeiro" , "(21 96719-5996");
+		  
+		  tc.visualizar();
+
+	
+		  Exer01_ClasseCliente  tc1 = new Exer01_ClasseCliente ("Jamile" , 18, "Feminino" , "Rio de Janeiro" , "(21 95734-5959");
+		  
+		  tc1.visualizar();
+
+	}
+}
+
+
---
+++ orientacao_objeto/src/orientacao_objeto/TestaFuncionario.java
@@ -0,0 +1,19 @@
+package orientacao_objeto;
+
+public class TestaFuncionario {
+
+	public static void main(String[] args) {
+		
+		  Exer02_ClasseFuncionario cf = new Exer02_ClasseFuncionario("Andresa" , 26, "COE" , "Engenheira de Software" , 5000.00f);
+		  
+		  cf.visualizar();
+
+	
+	     Exer02_ClasseFuncionario c1 = new Exer02_ClasseFuncionario("Heimy" , 19, "COE" , "Engenheira de Software" , 5000.00f);
+	  
+	     c1.visualizar();
+
+	 
+
+	}
+}
